f6b0d2335ebf2f119f656f1d038629181acee085
oltionchampari <oltion.champari@datavisyn.io>
2021-08-04 15:31:55 +0200

    Fix linter

--- dist/pages/config.js
+++ dist/pages/config.js
@@ -1,19 +1,19 @@
 export const headerNavigationLinks = [
     {
-        text: "What's new",
-        page: "/news"
+        text: 'What\'s new',
+        page: '/news'
     },
     {
-        text: "Features",
-        page: "/features"
+        text: 'Features',
+        page: '/features'
     },
     {
-        text: "Datasets",
-        page: "/datasets"
+        text: 'Datasets',
+        page: '/datasets'
     },
     {
-        text: "Help",
-        page: "/help"
+        text: 'Help',
+        page: '/help'
     }
 ];
 //# sourceMappingURL=config.js.map
