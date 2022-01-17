ab547b975d3ea1fb3319a3e3a0bee91cde118d85
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2022-01-17 12:42:22 +0100

    update dist

--- dist/pages/components/TermsOfUseCard.d.ts
+++ dist/pages/components/TermsOfUseCard.d.ts
@@ -1,3 +1,2 @@
-/// <reference types="react" />
 import { IHelpPageCardProps } from './OrdinoHelpSection';
 export declare function TermsOfUseCard({ openInNewWindow }: IHelpPageCardProps): JSX.Element;
--- dist/pages/components/VideoCard.d.ts
+++ dist/pages/components/VideoCard.d.ts
@@ -1,3 +1,2 @@
-/// <reference types="react" />
 import { IHelpPageCardProps } from './OrdinoHelpSection';
 export declare function VideoCard({ openInNewWindow }: IHelpPageCardProps): JSX.Element;
