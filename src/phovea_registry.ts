96da268c42b59c1d2b88cb516aecd01061490894
dvdanielamoitzi <57343176+dvdanielamoitzi@users.noreply.github.com>
2024-02-29 13:55:02 +0100

    chore: version pinning (#157)
    version pinning

--- src/phovea_registry.ts
+++ src/phovea_registry.ts
@@ -10,8 +10,6 @@ import reg from './phovea';
  * build a registry by registering all phovea modules
  */
 // other modules
-import 'visyn_core/phovea_registry';
-import 'tdp_core/dist/phovea_registry';
 import 'ordino/dist/phovea_registry';
 // self
 PluginRegistry.getInstance().register('ordino_public', reg);
